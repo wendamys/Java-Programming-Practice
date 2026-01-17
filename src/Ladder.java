333afe0189235e15baeab43555e0d3cc83a48f5e
wendamys <wendamys@yandex.ru>
2026-01-17 22:39:33 +0700

    add Ladder

---
+++ src/Ladder.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+// test input -> 5
+// output -> 1
+           //1 2
+           //1 2 2
+           //1 2 2 3
+           //1 2 2 3 3
+
+class Ladder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int k = 0;
+        int s = 1;
+        int flag = 0;
+        while (a >= s) {
+            for (int i = 1; a >= i; i++) {
+                for (int j = 1; i >= j; j++) {
+                    if (s > k) {
+                        System.out.print((s == (k+1) ? i : i + " "));
+                        //System.out.print(i + " ");
+                        k++;
+                    }
+                }
+            }
+            k = 0;
+            System.out.print("\n");
+            s++;
+        }
+    }
+}
